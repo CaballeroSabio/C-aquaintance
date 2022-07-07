1571780ee612e1f7e574a698025f2f70407ce41c
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-08 01:32:54 +0300

    0

---
+++ 05_IfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Enter username: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "maria")
+{
+    Console.WriteLine("Oh, yes! This is Maria");
+}
+else
+{
+    Console.Write("Hi!");
+    Console.WriteLine(username);
+}
