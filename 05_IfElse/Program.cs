0e7141ec3d99d4b874d7d549c36c27ae858f3846
Andrew Glinsky <glinsky_andrew@hotmail.com>
2022-07-10 14:17:42 +0300

    added Console.Clear()

--- 05_IfElse/Program.cs
+++ 05_IfElse/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Enter username: ");
+Console.Clear();
+Console.WriteLine("Hi, User!");
+Console.Write("Enter your name, my friend: ");
 string username = Console.ReadLine();
 
 if(username.ToLower() == "maria")
